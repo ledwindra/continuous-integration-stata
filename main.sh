d50496c26189fb779e19153aa17309b3b72c3a0b
Lukman Edwindra <lukman.edwindra@gmail.com>
2021-04-21 11:22:56 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,32 +9,34 @@ SERIAL_NUMBER=$6
 URL=$7
 VERSION=$8
 
-mkdir ~/Downloads
-cd ~/Downloads
-wget $URL
-mv $OLD_FILE Stata${VERSION}Linux64.tar.gz
-cd /tmp/
-sudo mkdir statafiles
-cd statafiles
-sudo tar -zxf ~/Downloads/Stata${VERSION}Linux64.tar.gz
-cd /usr/local
-sudo mkdir stata${VERSION}
-cd stata${VERSION}
-sudo yes | sudo /tmp/statafiles/install
-export PATH=/usr/local/stata${VERSION}:$PATH
-sudo ./stinit << EOF
-y
-y
-$SERIAL_NUMBER
-$CODE
-$AUTHORIZATION
-y
-y
-$FIRST_NAME
-$LAST_NAME
-y
-EOF
-which stata
-cd ~
-/usr/local/stata${VERSION}/stata -b do main
-cat main.log
+echo "$URL"
+
+# mkdir ~/Downloads
+# cd ~/Downloads
+# wget $URL
+# mv $OLD_FILE Stata${VERSION}Linux64.tar.gz
+# cd /tmp/
+# sudo mkdir statafiles
+# cd statafiles
+# sudo tar -zxf ~/Downloads/Stata${VERSION}Linux64.tar.gz
+# cd /usr/local
+# sudo mkdir stata${VERSION}
+# cd stata${VERSION}
+# sudo yes | sudo /tmp/statafiles/install
+# export PATH=/usr/local/stata${VERSION}:$PATH
+# sudo ./stinit << EOF
+# y
+# y
+# $SERIAL_NUMBER
+# $CODE
+# $AUTHORIZATION
+# y
+# y
+# $FIRST_NAME
+# $LAST_NAME
+# y
+# EOF
+# which stata
+# cd ~
+# /usr/local/stata${VERSION}/stata -b do main
+# cat main.log
